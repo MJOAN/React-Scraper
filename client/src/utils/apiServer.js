7df0e8a9427b3642683661ded8b02354f1fa844d
Mariam Joan <royalton@gmail.com>
2018-02-15 19:22:47 -0800

    heroku added heroku URLas end point

--- client/src/utils/apiServer.js
+++ client/src/utils/apiServer.js
@@ -3,14 +3,14 @@ import axios from "axios";
 export default {
     // Gets all articles saved from database
     savedArticles: function() {
-        return axios.get("/api/articles");
+        return axios.get("https://lit-hollows-89451.herokuapp.com/" + "/api/articles");
     },
     // Gets the articles with the given id
     saveArticle: function(id) {
-        return axios.post("/api/articles/" + id);
+        return axios.post("https://lit-hollows-89451.herokuapp.com/" + "/api/articles/" + id);
     },
     // Deletes the articles with the given id from database
     deleteArticle: function(id) {
-        return axios.delete("/api/articles/" + id);
+        return axios.delete("https://lit-hollows-89451.herokuapp.com/" + "/api/articles/" + id);
     }
 };
