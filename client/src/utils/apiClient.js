d174f7bfc8bd72f90ef0c1d7dbe33209bb2a7d11
Mariam Joan <royalton@gmail.com>
2018-09-19 23:24:48 -0700

    add https mixed content

--- client/src/utils/apiClient.js
+++ client/src/utils/apiClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://api.nytimes.com/svc/search/v2/articlesearch.json?q=";
+const baseURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=";
 const apikey = "&api-key=f28ae2254f6244cf88f9de98174f39e9";
 
 
